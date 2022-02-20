da1531853dc8a31d1158c6d7a234cfa0d6ea429a
Tester <branislav985@gmail.com>
2022-02-20 21:21:14 +0100

    test2

--- src/test/java/selenium/SeleniumTest.java
+++ src/test/java/selenium/SeleniumTest.java
@@ -1,31 +1,27 @@
 package selenium;
-import org.junit.Test;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.BeforeClass;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumTest {
 	
 	
 
-	class ChromeTest {
-
-	    
-
-	}
 @BeforeClass
 public static void setupClass() {
 	WebDriverManager.chromedriver().setup();
 }
 
 @Test
-public void smartTest() {
+public void smartTest() throws InterruptedException {
 	WebDriver driver = new ChromeDriver();
 	driver.get("https:/www.wikipedia.com/");
+		Thread.sleep(3000);
 	driver.quit();
-	
+
+
 }
 }
